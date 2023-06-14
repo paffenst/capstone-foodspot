9ecfe64a126061804ba2ef7ff06946a17de3de9b
paffenst <paffen.st@gmail.com>
2023-06-14 15:18:24 +0200

    add food-map controller,service,api und tsx

---
+++ backend/src/main/java/de/neuefische/backend/controllers/FoodMapController.java
@@ -0,0 +1,22 @@
+package de.neuefische.backend.controllers;
+
+import de.neuefische.backend.services.MapService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/mapbox")
+public class FoodMapController {
+
+    private final MapService mapService;
+
+    public FoodMapController(MapService mapService) {
+        this.mapService = mapService;
+    }
+
+    @GetMapping
+    public String getToken(){
+        return mapService.getToken();
+    }
+}
---
+++ frontend/src/components/HomePage.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Map from "../components/foodmap/Map"
+import {Box} from "@mui/material";
+
+type HomepageProps = {
+    token: string
+}
+export default function HomePage(props: HomepageProps) {
+    return (
+        <div>
+            <> Food Spot App </>
+            <Box>
+                <Map token={props.token}/>
+            </Box>
+        </div>
+    )
+}
