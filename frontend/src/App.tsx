3d0563ba4cbbcfd0e7d587a651a6ed252cf0c1a9
paffenst <paffen.st@gmail.com>
2023-06-15 18:04:44 +0200

    add frontend header , make some changes on homepage

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import useAccessToken from "./hooks/useAccessToken";
 import HomePage from "./components/HomePage";
+import Header from "./components/header/Header";
 
 export default function App() {
     const {token} = useAccessToken()
     return (
-        <h4>
+       <div>
+            <Header/>
             <HomePage token={token}/>
-        </h4>
+       </div>
     )
 }
