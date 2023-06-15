b09c9c3bdf10e8f81069b5975ac33a8aad4d8066
paffenst <paffen.st@gmail.com>
2023-06-15 12:13:28 +0200

    hotfix

--- backend/src/test/java/de/neuefische/backend/MapServiceTest.java
+++ backend/src/test/java/de/neuefische/backend/MapServiceTest.java
@@ -5,13 +5,13 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-public class MapServiceTest {
-
+class MapServiceTest {
     MapService mapService = new MapService();
+
     @Test
     void getToken_expect() {
         String expected = System.getenv("mapToken");
         String result = mapService.getToken();
-        assertEquals(expected,result);
+        assertEquals(expected, result);
     }
 }
