f6e757d9979a631cd7a9860c56de11a38fc30940
paffenst <paffen.st@gmail.com>
2023-06-21 00:55:02 +0200

    add User Repo in backend

---
+++ backend/src/main/java/de/neuefische/backend/repos/UserRepo.java
@@ -0,0 +1,12 @@
+package de.neuefische.backend.repos;
+
+import de.neuefische.backend.models.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserRepo extends MongoRepository<User,String> {
+    Optional<User> findMongoUserByUsername(String username);
+}
