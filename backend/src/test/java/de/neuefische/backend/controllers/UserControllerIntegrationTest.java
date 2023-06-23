a3ba6d28f563dfdfa726e4b7d0135cd5f748c9c1
paffenst <paffen.st@gmail.com>
2023-06-23 19:29:45 +0200

    service and controller user test

---
+++ backend/src/test/java/de/neuefische/backend/controllers/UserControllerIntegrationTest.java
@@ -0,0 +1,64 @@
+package de.neuefische.backend.controllers;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.neuefische.backend.models.UserDTO;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+class UserControllerIntegrationTest {
+    @Autowired
+    MockMvc mockMvc;
+
+    @Test
+    @DirtiesContext
+    void registerUser_thenStatus200() throws Exception {
+        UserDTO newUser = UserDTO.builder()
+                .username("testuser")
+                .password("testpass")
+                .email("test@test.de")
+                .firstname("test")
+                .lastname("test")
+                .build();
+
+        ObjectMapper objectMapper = new ObjectMapper();
+        String jsonRequestBody = objectMapper.writeValueAsString(newUser);
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/register")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(jsonRequestBody)
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(content().json("""
+                            {
+                                "username":"testuser",
+                                "email":"test@test.de",
+                                "firstname":"test",
+                                "lastname":"test"
+                            }
+                        """));
+    }
+
+    @Test
+    @DirtiesContext
+    @WithMockUser(username = "test", password = "testpass")
+    void loginUser_thenReturnStatus200() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/login")
+                        .with(csrf()))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().string("test"));
+    }
+}
---
+++ backend/src/test/java/de/neuefische/backend/services/MongoUserServiceTest.java
@@ -0,0 +1,62 @@
+package de.neuefische.backend.services;
+
+import de.neuefische.backend.exceptions.UserNotFoundException;
+import de.neuefische.backend.models.User;
+import de.neuefische.backend.models.UserDTO;
+import de.neuefische.backend.models.UserNoAuth;
+import de.neuefische.backend.repos.UserRepo;
+import org.junit.jupiter.api.Test;
+import org.springframework.test.annotation.DirtiesContext;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+class MongoUserServiceTest {
+
+    UserRepo userRepo = mock(UserRepo.class);
+    IdService idService = mock(IdService.class);
+    EncodePassBCryptEncoderService hashPasswordService = mock(EncodePassBCryptEncoderService.class);
+    MongoUserService mongoService = new MongoUserService(userRepo, idService, hashPasswordService);
+
+
+    @Test
+    void loadUserByUsername_expect_MyUsernameNotFoundException() {
+        when(userRepo.findByUsername("paff")).thenThrow(new UserNotFoundException());
+    }
+
+    @DirtiesContext
+    @Test
+    void registerUser_returnNewUser() {
+
+        String userTest = "test";
+        String idTest = "idtest";
+        String passTest = "passtest";
+        String hashpass = "hashpass";
+
+        UserDTO newUser = UserDTO.builder()
+                .username(userTest)
+                .password(passTest)
+                .build();
+        User userNew = User.builder()
+                .id(idTest)
+                .username(userTest)
+                .password(hashpass)
+                .build();
+
+        when(idService.generateId()).thenReturn(idTest);
+        when(hashPasswordService.encodedPassword(newUser)).thenReturn(hashpass);
+        when(userRepo.save(userNew)).thenReturn(userNew);
+
+        UserNoAuth expectedUser = UserNoAuth.builder()
+                .username(userTest)
+                .build();
+        UserNoAuth actualUser = mongoService.registerUser(newUser);
+
+        //then//when
+        assertEquals(expectedUser, actualUser);
+        verify(idService).generateId();
+        verify(hashPasswordService).encodedPassword(newUser);
+        verify(userRepo).save(userNew);
+
+    }
+}
