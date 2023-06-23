390fb9c4c2d998967ce873d3b5bf7120a56400fe
paffenst <paffen.st@gmail.com>
2023-06-23 19:36:46 +0200

    add user controller

--- backend/src/main/java/de/neuefische/backend/controllers/UserController.java
+++ backend/src/main/java/de/neuefische/backend/controllers/UserController.java
@@ -1,20 +1,32 @@
 package de.neuefische.backend.controllers;
 
-import de.neuefische.backend.models.User;
 import de.neuefische.backend.models.UserDTO;
+import de.neuefische.backend.models.UserNoAuth;
 import de.neuefische.backend.services.MongoUserService;
-
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import org.springframework.web.bind.annotation.*;
 @RestController
-@RequestMapping("user")
+@RequestMapping("api")
 @RequiredArgsConstructor
 public class UserController {
     private final MongoUserService userService;
 
     @PostMapping("/register")
-    public User registerUser(@RequestBody UserDTO userDTO) {
-        return userService.registerUser(userDTO);
+    public UserNoAuth registerUser(@RequestBody UserDTO newUser) {
+        return userService.registerUser(newUser);
+    }
+
+    @PostMapping("/login")
+    public String login() {
+        System.out.println(SecurityContextHolder.getContext());
+        return SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getName();
     }
 }
