255aa28ac01aa2b96218fc794ca81c389528d1e1
paffenst <paffen.st@gmail.com>
2023-06-29 11:52:11 +0200

    add mui elements on a map with some usestates

--- frontend/src/components/HomePage.tsx
+++ frontend/src/components/HomePage.tsx
@@ -1,15 +1,97 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Map from "../components/foodmap/Map"
-import {Box} from "@mui/material";
+import {Box, Drawer, Fab, SpeedDial, SpeedDialAction} from "@mui/material";
+import {Foodlocation} from "../models/Foodlocation";
+import mapboxgl from "mapbox-gl";
+import {Add, FoodBank, LocationOn} from "@mui/icons-material";
+import AddFoodSpot from "./foodmap/AddFoodSpot";
 
 type HomepageProps = {
     token: string
 }
 export default function HomePage(props: HomepageProps) {
+    const [openDrawer, setOpenDrawer] = useState<boolean>(false)
+    const [markedLocation, setMarkedLocation] = useState<Foodlocation>({latitude: 0, longitude: 0})
+    const [centerMarker, setCenterMarker] = useState<mapboxgl.Marker>()
+    const [hideMarkLocation, setHideMarkLocation] = useState(true)
+
+    function handleCurrentPosition() {
+        navigator.geolocation.getCurrentPosition((position) => {
+            setMarkedLocation({
+                ...markedLocation,
+                latitude: position.coords.latitude,
+                longitude: position.coords.longitude
+            })
+            setOpenDrawer(true)
+        })
+    }
+
+    function handleDrawerClose() {
+        setOpenDrawer(false)
+    }
+
+    function handleSaveSpot() {
+        setOpenDrawer(false)
+        setHideMarkLocation(true)
+        centerMarker?.remove()
+        setCenterMarker(undefined)
+    }
+
+    function handleCreateCenterMarker() {
+        if (!centerMarker) {
+            setCenterMarker(new mapboxgl.Marker().setLngLat([0, 0]))
+            setHideMarkLocation(false)
+        }
+    }
+
+    function handleChoosePosition() {
+        if (centerMarker?.getLngLat()) {
+            setMarkedLocation(
+                {
+                    ...markedLocation,
+                    latitude: centerMarker?.getLngLat().lat, longitude: centerMarker?.getLngLat().lng
+                })
+            setOpenDrawer(true)
+        }
+    }
+
     return (
         <div>
-            <Box >
-                <Map token={props.token}/>
+            <Box>
+                <Map choosePositionMarker={centerMarker} token={props.token}/>
+                {!hideMarkLocation &&
+                    <Fab color={"success"} variant={"extended"} hidden={true} onClick={handleChoosePosition}
+                         sx={{
+                             left: 50,
+                             position: 'fixed',
+                             bottom: 40
+                         }}>
+                        <FoodBank style={{color: "gold"}} sx={{mr: 1}}/>
+                        Mark location
+                    </Fab>}
+                <SpeedDial
+                    ariaLabel={"addFoodSpot"}
+                    sx={{
+                        right: 50,
+                        position: 'fixed',
+                        bottom: 40
+                    }}
+                    icon={<Add/>}
+                >
+                    <SpeedDialAction key={"chooselocation"}
+                                     icon={<LocationOn/>}
+                                     tooltipOpen={true}
+                                     tooltipTitle={"Mark a position"}
+                                     onClick={handleCreateCenterMarker}
+                    />
+                </SpeedDial>
+
+                <Drawer
+                    anchor={"top"}
+                    open={openDrawer}>
+                    <AddFoodSpot markedLocation={markedLocation} handleCancel={handleDrawerClose}
+                                 handleSave={handleSaveSpot}/>
+                </Drawer>
             </Box>
         </div>
     )
