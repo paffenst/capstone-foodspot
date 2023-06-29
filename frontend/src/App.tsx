2a5503dc661fd1e378b6e46e975969ef6bfaef89
paffenst <paffen.st@gmail.com>
2023-06-29 19:08:56 +0200

    Add route for spots

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import useAccessToken from "./hooks/useAccessToken";
+import useAddFoodSpot from "./hooks/useAddFoodSpot";
 import HomePage from "./components/HomePage";
 import {Route, Routes} from 'react-router-dom';
 import Header from "./components/header/Header";
@@ -8,13 +9,14 @@ import RegisterPage from "./components/user/RegisterPage";
 
 export default function App() {
     const {token} = useAccessToken()
+    const {addFoodSpot} = useAddFoodSpot()
     return (
         <div>
             <div>
                 <Routes>
                     <Route path="/" element={<>
                         <Header/>
-                        <HomePage token={token}/>
+                        <HomePage handleSaveSpot={addFoodSpot} token={token}/>
                     </>
                     }
                     />
