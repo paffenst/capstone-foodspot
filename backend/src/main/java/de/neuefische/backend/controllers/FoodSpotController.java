76f86b277f40ef69d5942af909799a9ff77d78a6
paffenst <paffen.st@gmail.com>
2023-07-06 16:07:55 +0200

    add foodspot backend (controller, service)

--- backend/src/main/java/de/neuefische/backend/controllers/FoodSpotController.java
+++ backend/src/main/java/de/neuefische/backend/controllers/FoodSpotController.java
@@ -4,10 +4,11 @@ import de.neuefische.backend.models.FoodSpot;
 import de.neuefische.backend.models.FoodSpotDTO;
 import de.neuefische.backend.services.FoodSpotService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("food-spots")
@@ -18,5 +19,15 @@ public class FoodSpotController {
     @PostMapping("/add-spot")
     public FoodSpot addSpot(@RequestBody FoodSpotDTO newFoodSpot) {
         return foodSpotService.add(newFoodSpot);
+    }
+
+    @GetMapping("/allFoodSpots")
+    public ResponseEntity<List<FoodSpot>> listSpots() {
+        return new ResponseEntity<>(foodSpotService.list(), HttpStatus.OK);
+    }
+
+    @GetMapping("/allFoodSpots/{id}")
+    public ResponseEntity<FoodSpot> byId(@PathVariable String id) {
+        return new ResponseEntity<>(foodSpotService.getById(id), HttpStatus.OK);
     }
 }
--- backend/src/main/java/de/neuefische/backend/services/FoodSpotService.java
+++ backend/src/main/java/de/neuefische/backend/services/FoodSpotService.java
@@ -1,11 +1,13 @@
 package de.neuefische.backend.services;
 
+import de.neuefische.backend.exceptions.NoSuchFoodSpotException;
 import de.neuefische.backend.models.FoodSpot;
 import de.neuefische.backend.models.FoodSpotDTO;
 import de.neuefische.backend.repos.FoodSpotRepo;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 @Service
 @RequiredArgsConstructor
 public class FoodSpotService {
@@ -22,5 +24,13 @@ public class FoodSpotService {
                 newFoodSpot.placeType(),
                 newFoodSpot.position());
         return foodSpotRepo.save(newFoodSpotWithID);
+    }
+
+    public List<FoodSpot> list() {
+        return foodSpotRepo.findAll();
+    }
+
+    public FoodSpot getById(String id) {
+        return foodSpotRepo.findById(id).orElseThrow(NoSuchFoodSpotException::new);
     }
 }
