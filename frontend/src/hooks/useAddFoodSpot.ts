27d73d6baad102b958760cabe82e58f37597b488
paffenst <paffen.st@gmail.com>
2023-07-11 15:17:54 +0200

    add a use Callback

--- frontend/src/hooks/useAddFoodSpot.ts
+++ frontend/src/hooks/useAddFoodSpot.ts
@@ -1,9 +1,17 @@
 import axios from "axios";
 import {FoodSpot} from "../models/FoodSpot";
-import {useEffect, useState} from "react";
+import {useCallback, useEffect, useState} from "react";
 
 export default function useAddFoodSpot() {
     const [foodSpot, setFoodSpot] = useState<FoodSpot[]>([]);
+
+    const getAllFoodSpots = useCallback(() => {
+        return axios.get("/food-spots/allFoodSpots").then((response) => response.data);
+    }, []);
+
+    const getFoodSpots = useCallback(() => {
+        getAllFoodSpots().then((data) => setFoodSpot(data));
+    }, [getAllFoodSpots]);
 
     useEffect(() => {
         getFoodSpots();
@@ -13,15 +21,6 @@ export default function useAddFoodSpot() {
         return axios
             .post("/food-spots/add-spot", newFoodSpot)
             .then((response) => setFoodSpot([...foodSpot, response.data]));
-    }
-
-    function getAllFoodSpots(): Promise<FoodSpot[]> {
-        return axios.get("/food-spots/allFoodSpots").then((response) => response.data);
-    }
-
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    function getFoodSpots(): void {
-        getAllFoodSpots().then((data) => setFoodSpot(data));
     }
 
     return {foodSpot, addFoodSpot, getFoodSpots, setFoodSpot};
