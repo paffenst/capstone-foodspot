fc404d86f2f42e878a940fa43a9b7842a142b6da
paffenst <paffen.st@gmail.com>
2024-05-08 14:02:09 +0200

    quick change header

--- frontend/src/components/header/Header.tsx
+++ frontend/src/components/header/Header.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import {AppBar, Button, Toolbar} from '@mui/material'
+import {AppBar, Button, Toolbar, Typography} from '@mui/material';
 import foodspotterslogo from '../../images/FS_green_rounded_final_logo.png';
-import {NavLink, useNavigate} from "react-router-dom";
+import {NavLink, useNavigate} from 'react-router-dom';
 
 export default function Header() {
     const navigate = useNavigate()
 
     function onClickRegisterHandler() {
-        navigate("/register");
+        navigate('/register');
     }
 
     function onClickLoginHandler() {
@@ -20,31 +20,39 @@ export default function Header() {
 
     return (
         <AppBar position="static">
-            <Toolbar className="Toolbar">
-                <NavLink style={{flexGrow: 10}} to="/login">
-                    <img src={foodspotterslogo} alt="Food-Spot-App Logo"
-                         style={{height: '50px', verticalAlign: 'middle'}}/>
-                </NavLink>
-                <div>
-                    <Button onClick={onClickLoginHandler}
-                            style={{height: '50%', backgroundColor: 'aquamarine', color: 'black'}}
-                            variant="contained">
+            <Toolbar className="Toolbar" style={{display: 'flex', justifyContent: 'space-between'}}>
+                <div style={{display: 'flex', alignItems: 'center'}}>
+                    <NavLink to="/login">
+                        <img src={foodspotterslogo} alt="Food-Spot-App Logo"
+                             style={{height: '50px', verticalAlign: 'middle'}}/>
+                    </NavLink>
+                    <Typography variant="body1" style={{marginLeft: '10px', color: 'white'}}>
+                        Logged in as:
+                    </Typography>
+                </div>
+                <div style={{display: 'flex', alignItems: 'center'}}>
+                    <Button
+                        onClick={onClickLoginHandler}
+                        style={{height: '50%', backgroundColor: 'aquamarine', color: 'black', marginLeft: '10px'}}
+                        variant="contained"
+                    >
                         Sign In
                     </Button>
-                </div>
-                <div>
-                    <Button onClick={onClickRegisterHandler}
-                            style={{height: '50%', backgroundColor: 'floralwhite ', color: 'black'}}
-                            variant="contained">
+                    <Button
+                        onClick={onClickRegisterHandler}
+                        style={{height: '50%', backgroundColor: 'floralwhite', color: 'black', marginLeft: '10px'}}
+                        variant="contained"
+                    >
                         Sign Up
                     </Button>
-                </div>
-                <div>
-                    <Button onClick={onClickLogoutHandler}
-                            style={{height: '50%', backgroundColor: '#d22e2e', color: 'black'}}
-                            variant="contained">
-                        Sign Out
-                    </Button>
+                    <div>
+                        <Button onClick={onClickLogoutHandler}
+                                style={{height: '50%', backgroundColor: '#d22e2e', color: 'black', marginLeft: '10px'}}
+                                variant="contained"
+                        >
+                            Sign Out
+                        </Button>
+                    </div>
                 </div>
             </Toolbar>
         </AppBar>
