ed32339e7c2ee8bacb46573b7548c4856bfb8fe3
paffenst <paffen.st@gmail.com>
2024-07-22 14:25:00 +0200

    configure CORS in the controller

--- backend/src/main/java/de/neuefische/backend/controllers/FoodMapController.java
+++ backend/src/main/java/de/neuefische/backend/controllers/FoodMapController.java
@@ -1,12 +1,14 @@
 package de.neuefische.backend.controllers;
 
 import de.neuefische.backend.services.MapService;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api/mapbox")
+@CrossOrigin(origins = "https://capstone-foodspot.onrender.com/")
 public class FoodMapController {
     private final MapService mapService;
 
