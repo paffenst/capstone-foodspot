b82bbf8331831d30d7565f759ae1f29a2592cca2
paffenst <paffen.st@gmail.com>
2025-06-07 11:52:13 +0200

    adding search place logic- frontend

--- backend/src/main/java/de/neuefische/backend/repos/FoodSpotRepo.java
+++ backend/src/main/java/de/neuefische/backend/repos/FoodSpotRepo.java
@@ -4,7 +4,11 @@ import de.neuefische.backend.models.FoodSpot;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface FoodSpotRepo extends MongoRepository<FoodSpot, String> {
+    List<FoodSpot> findByNameContainingIgnoreCase(String name);
 
+    List<FoodSpot> findByNameContainingIgnoreCaseAndPlaceType(String query, String placeType);
 }
